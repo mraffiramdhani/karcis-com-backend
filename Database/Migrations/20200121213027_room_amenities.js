409e20966f6bb93ea6c4e21cf2047511e32dfc56
mraffiramdhani <raffi.programming@gmail.com>
2020-01-23 13:51:56 +0700

    Adding Room Amenities Seeder

--- Database/Migrations/20200121213027_room_amenities.js
+++ Database/Migrations/20200121213027_room_amenities.js
@@ -4,7 +4,7 @@ exports.up = (knex) => knex.schema.createTable('room_amenities', (table) => {
   table.integer('amenities_id').unsigned();
   table.integer('hotel_id').unsigned();
   table.integer('room_type_id').unsigned();
-  table.decimal('cost').defaultTo(0);
+  table.decimal('cost', 12, 2).defaultTo(0);
 
   table.foreign('amenities_id').references('amenities.id').onDelete('cascade').onUpdate('cascade');
   table.foreign('hotel_id').references('hotels.id').onDelete('cascade').onUpdate('cascade');

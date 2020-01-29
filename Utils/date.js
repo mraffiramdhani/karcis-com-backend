bed35b5343b7ec7543cc26eb35efb97a536bbb01
mraffiramdhani <raffi.programming@gmail.com>
2020-01-29 14:52:51 +0700

    Minor Update

--- Utils/date.js
+++ Utils/date.js
@@ -9,15 +9,10 @@ const dateRange = (start, end) => {
 };
 
 const convertDate = (strDate) => {
-	const dt = new Date(strDate);
+	var dt = new Date(strDate);
 
-	return `${
-    (dt.getMonth()+1).toString().padStart(2, '0')}/${
-    dt.getDate().toString().padStart(2, '0')}/${
-    dt.getFullYear().toString().padStart(4, '0')} ${
-    dt.getHours().toString().padStart(2, '0')}:${
-    dt.getMinutes().toString().padStart(2, '0')}:${
-    dt.getSeconds().toString().padStart(2, '0')}`
+	var str =  `${dt.getFullYear().toString().padStart(4, '0')}-${(dt.getMonth()+1).toString().padStart(2, '0')}-${dt.getDate().toString().padStart(2, '0')} ${dt.getHours().toString().padStart(2, '0')}:${dt.getMinutes().toString().padStart(2, '0')}:${dt.getSeconds().toString().padStart(2, '0')}`;
+	return str;
 }
 
 module.exports = {
